6310499144017597ffbb613921456e1840ffe71b
WeiChunnn <tanweichun@outlook.com>
2020-09-17 00:39:10 +0800

    Created Experience Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import LandingPage from './components/LandingPage';
 import Education from './components/Education';
+import Experience from './components/Experience';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <Fragment>
       <LandingPage />
       <Education />
+      <Experience />
     </Fragment>
   );
 }
---
+++ src/components/Experience.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Experience = () => {
+  return (
+    <div class='container'>
+      <div class='grid-2'>
+        <div className='section-title'>
+          <h1>Experience</h1>
+        </div>
+        <div class='information'>
+          <div>
+            <h1>GIS Cyber Security Engineer Intern</h1>
+            <h2>Alliance Bank Malaysia Berhad · August 2020 - Present</h2>
+            <h3>
+              <ul>
+                <li>
+                  Collaborated with a team to introduce Identity Access
+                  Management system into current business process.
+                </li>
+                <li>
+                  Assisted in producing a detailed Scenario Analysis for SWIFT
+                  Payment System in accordance to BNM and RMiT regulations.
+                </li>
+                <li>
+                  Refactored and consolidated 4 SOPs addressing System IDs
+                  Management and User Access Matrix.
+                </li>
+              </ul>
+            </h3>
+          </div>
+          <div>
+            <h1>Peer Assisted Study Session (PASS) Leader </h1>
+            <h2>INTI International College Subang · August 2019 - May 2020</h2>
+            <h3>
+              <ul>
+                <li>
+                  Provided weekly two-hour discussion session for General
+                  Chemistry I students.
+                </li>
+                <li>
+                  Developed customized lesson plans and worksheets to help study
+                  group members understand concepts to solve problems
+                </li>
+                <li>Raised the average grades of students from B- to A.</li>
+              </ul>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
