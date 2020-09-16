f252120116a4ccb14d361e1b315ef193ebe1d6cb
WeiChunnn <tanweichun@outlook.com>
2020-09-17 01:42:51 +0800

    Added bottom seperator

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,53 +1,58 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Experience = () => {
   return (
-    <div class='container'>
-      <div class='grid-2'>
-        <div className='section-title'>
-          <h1>Experience</h1>
-        </div>
-        <div class='information'>
-          <div>
-            <h1>GIS Cyber Security Engineer Intern</h1>
-            <h2>Alliance Bank Malaysia Berhad · August 2020 - Present</h2>
-            <h3>
-              <ul>
-                <li>
-                  Collaborated with a team to introduce Identity Access
-                  Management system into current business process.
-                </li>
-                <li>
-                  Assisted in producing a detailed Scenario Analysis for SWIFT
-                  Payment System in accordance to BNM and RMiT regulations.
-                </li>
-                <li>
-                  Refactored and consolidated 4 SOPs addressing System IDs
-                  Management and User Access Matrix.
-                </li>
-              </ul>
-            </h3>
+    <Fragment>
+      <div class='container'>
+        <div class='grid-2'>
+          <div className='section-title'>
+            <h1>Experience</h1>
           </div>
-          <div>
-            <h1>Peer Assisted Study Session (PASS) Leader </h1>
-            <h2>INTI International College Subang · August 2019 - May 2020</h2>
-            <h3>
-              <ul>
-                <li>
-                  Provided weekly two-hour discussion session for General
-                  Chemistry I students.
-                </li>
-                <li>
-                  Developed customized lesson plans and worksheets to help study
-                  group members understand concepts to solve problems
-                </li>
-                <li>Raised the average grades of students from B- to A.</li>
-              </ul>
-            </h3>
+          <div class='information'>
+            <div>
+              <h1>GIS Cyber Security Engineer Intern</h1>
+              <h2>Alliance Bank Malaysia Berhad · August 2020 - Present</h2>
+              <h3>
+                <ul>
+                  <li>
+                    Collaborated with a team to introduce Identity Access
+                    Management system into current business process.
+                  </li>
+                  <li>
+                    Assisted in producing a detailed Scenario Analysis for SWIFT
+                    Payment System in accordance to BNM and RMiT regulations.
+                  </li>
+                  <li>
+                    Refactored and consolidated 4 SOPs addressing System IDs
+                    Management and User Access Matrix.
+                  </li>
+                </ul>
+              </h3>
+            </div>
+            <div>
+              <h1>Peer Assisted Study Session (PASS) Leader </h1>
+              <h2>
+                INTI International College Subang · August 2019 - May 2020
+              </h2>
+              <h3>
+                <ul>
+                  <li>
+                    Provided weekly two-hour discussion session for General
+                    Chemistry I students.
+                  </li>
+                  <li>
+                    Developed customized lesson plans and worksheets to help
+                    study group members understand concepts to solve problems
+                  </li>
+                  <li>Raised the average grades of students from B- to A.</li>
+                </ul>
+              </h3>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div class='border'></div>
+    </Fragment>
   );
 };
 
