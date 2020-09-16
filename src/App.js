9aba19da0a896f0162bffbef89f2ea23fc0b5b1d
WeiChunnn <tanweichun@outlook.com>
2020-09-17 00:29:15 +0800

    Created Education Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import LandingPage from './components/LandingPage';
+import Education from './components/Education';
+
 import './App.css';
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Fragment>
+      <LandingPage />
+      <Education />
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/Education.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Education = () => {
+  return (
+    <div class='container'>
+      <div class='grid-2'>
+        <div className='section-title'>
+          <h1>Education</h1>
+        </div>
+        <div class='information'>
+          <div>
+            <h1>INTI International College Subang</h1>
+            <h2>Bachelor of Science in Computer Science · December 2023</h2>
+            <h3>
+              <ul>
+                <li>
+                  Chosen to attend the prestigious LATAM Impact Program in 2019
+                  which selects only 10 individuals from the world's top
+                  schools.
+                </li>
+                <li>Cumulative GPA: 3.94/4.00</li>
+                <li>Dean's List: Spring 2019, Fall 2019</li>
+                <li>President's List: Spring 2020</li>
+              </ul>
+            </h3>
+          </div>
+          <div>
+            <h1>Wesley Methodist School Kuala Lumpur </h1>
+            <h2>
+              CGPA: 4.00/4.00; IGCSE: 10A*s; SPM: 1A+, 1A, 1B+ · November 2018
+            </h2>
+            <h3>
+              <ul>
+                <li>Scholar of the Year – Valedictorian </li>
+                <li>
+                  Outstanding Cambridge Learners Award (Additional Mathematics)
+                </li>
+                <li>Honor Roll Student (2014 - 2018)</li>
+                <li>
+                  Outstanding Award for Hua Lo Geng Mathematics Competition
+                </li>
+              </ul>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Education;
