46d21f8fcefc8434ad44cff6de1d03f81bda0814
WeiChunnn <tanweichun@outlook.com>
2020-09-17 01:28:01 +0800

    Updated with progress bar

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import ProgressBar from '../items/ProgressBar';
 
 const Skills = () => {
   return (
     <div class='container'>
-      <div class='grid-2'>
+      <div className='grid-2'>
         <div className='section-title'>
           <h1>Skills</h1>
         </div>
-        <div></div>
+        <div>
+          <ProgressBar title={'HTML5'} percentage={'75%'} />
+          <ProgressBar title={'CSS3'} percentage={'75%'} />
+          <ProgressBar title={'ReactJS'} percentage={'87.5%'} />
+          <ProgressBar title={'NodeJS'} percentage={'60%'} />
+          <ProgressBar title={'ExpressJS'} percentage={'70%'} />
+          <ProgressBar title={'Vue'} percentage={'20%'} />
+          <ProgressBar title={'Git'} percentage={'50%'} />
+          <ProgressBar title={'Python'} percentage={'75%'} />
+        </div>
       </div>
     </div>
   );
